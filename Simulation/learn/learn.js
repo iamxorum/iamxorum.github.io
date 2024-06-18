432f84f377916d0f1dd6624666db68691d403477
iamxorum <this.isxorum@gmail.com>
2024-06-18 20:17:23 +0300

    18.06.2024 - Added search Box Learning Mode + GUI improvements

--- Simulation/learn/learn.js
+++ Simulation/learn/learn.js
@@ -1,6 +1,9 @@
 (async () => {
   const loadingScreen = document.getElementsByClassName("loader_container");
   const quizContainer = document.getElementsByClassName("body");
+  const searchBox = document.getElementById("search-box");
+  let allQuestions = []; // To store all questions for the current subject
+
   try {
     const subjects = {
       ASD: "../assets/asd.json",
@@ -16,10 +19,8 @@
       SO: "../assets/so.json",
       TAP: "../assets/tap.json",
       WEB: "../assets/web.json",
-      //ORTO: "../assets/orto.json",
     };
 
-    // Function to fetch JSON from a file
     async function fetchJSONFile(filePath) {
       const response = await fetch(filePath);
       if (!response.ok) {
@@ -30,19 +31,15 @@
       return await response.json();
     }
 
-    // display the loading screen
     loadingScreen[0].style.display = "flex";
     quizContainer[0].style.display = "none";
 
-    //sleep randomize betwwen 1 and 5 seconds
     function sleep(ms) {
       return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
-    //sleep for 1 second
     await sleep(Math.floor(Math.random() * 5000) + 2000);
 
-    // Function to escape HTML entities to prevent XSS attacks
     function escapeHTML(str) {
       return str
         .replace(/&/g, "&amp;")
@@ -53,44 +50,13 @@
         .replace(/\n/g, "<br>");
     }
 
-    // Function to render questions
     function renderQuestions(questions, subject, isWeb) {
       const questionsContainer = document.getElementById("questions-container");
       questionsContainer.innerHTML = "";
 
       const subjectTitle = document.createElement("h2");
-      // Change the text content of the subject title to the selected subject
-      if (subject === "WEB") {
-        subjectTitle.textContent = `WEB - Tehnologii Web`;
-      } else if (subject === "ASD") {
-        subjectTitle.textContent = `ASD - Algoritmi și Structuri de Date`;
-      } else if (subject === "CC") {
-        subjectTitle.textContent = `CC - Cloud Computing`;
-      } else if (subject === "CE") {
-        subjectTitle.textContent = `CE - Comerț Electronic`;
-      } else if (subject === "C++") {
-        subjectTitle.textContent = `Programare C++`;
-      } else if (subject === "CRIPTO") {
-        subjectTitle.textContent = `CRIPTO - Criptografie`;
-      } else if (subject === "DB") {
-        subjectTitle.textContent = `DB - Baze de Date`;
-      } else if (subject === "JAVA") {
-        subjectTitle.textContent = `Programare Java`;
-      } else if (subject === "PP") {
-        subjectTitle.textContent = `PP - Programare Procedurală`;
-      } else if (subject === "RC") {
-        subjectTitle.textContent = `RC - Rețele de Calculatoare`;
-      } else if (subject === "SGDB") {
-        subjectTitle.textContent = `SGDB - Sisteme de Gestiune a Bazelor de Date`;
-      } else if (subject === "SO") {
-        subjectTitle.textContent = `SO - Sisteme de Operare`;
-      } else if (subject === "TAP") {
-        subjectTitle.textContent = `TAP - Tehnici Avansate de Programare`;
-      } else {
-        subjectTitle.textContent = `${subject}`;
-      }
+      subjectTitle.textContent = getSubjectTitle(subject);
       subjectTitle.id = `subject-${subject}`;
-      //center and margin 2rem
       subjectTitle.style.textAlign = "center";
       subjectTitle.style.marginTop = "2rem";
       subjectTitle.style.marginBottom = "2rem";
@@ -106,28 +72,21 @@
         questionDiv.classList.add("question-block");
         if (questionData.tobereviewd) {
           questionDiv.classList.add("tobereviewed");
-          // Add a a text block that says "To be reviewed by the teacher" to the question block in caps
           const reviewText = document.createElement("p");
           reviewText.textContent = "TREBUIE REVIZUIT DE PROFESOR";
-          questionDiv.appendChild(reviewText);
-
-          // add style to the reviewText
           reviewText.style.textAlign = "center";
           reviewText.style.color = "#000";
           reviewText.style.fontSize = "1.5rem";
           reviewText.style.fontWeight = "bold";
           reviewText.style.marginTop = "1rem";
           reviewText.style.marginBottom = "1rem";
+          questionDiv.appendChild(reviewText);
         }
 
         const questionText = document.createElement("h4");
-        if (isWeb) {
-          questionText.innerHTML = `Q${index + 1}: ${escapeHTML(
-            questionData.question
-          )}`;
-        } else {
-          questionText.innerHTML = `Q${index + 1}: ${questionData.question}`;
-        }
+        questionText.innerHTML = `Q${index + 1}: ${
+          isWeb ? escapeHTML(questionData.question) : questionData.question
+        }`;
         questionDiv.appendChild(questionText);
 
         if (questionData.img) {
@@ -140,46 +99,65 @@
         const answersList = document.createElement("ul");
         questionData.answers.forEach((answer, answerIndex) => {
           const answerItem = document.createElement("li");
-          if (isWeb) {
-            answerItem.innerHTML = escapeHTML(answer);
-          } else {
-            answerItem.innerHTML = answer;
-          }
+          answerItem.innerHTML = isWeb ? escapeHTML(answer) : answer;
           if (questionData.correctAnswerIndexes.includes(answerIndex)) {
             answerItem.classList.add("correct-answer");
           }
           answersList.appendChild(answerItem);
         });
         questionDiv.appendChild(answersList);
-
         questionsContainer.appendChild(questionDiv);
       });
     }
 
-    // Function to load and render questions for a selected subject
     async function loadSubject(subject) {
       const filePath = subjects[subject];
-      const questions = await fetchJSONFile(filePath);
-      let isWeb = false;
-      // if the subject is web, set isWeb to true
-      if (subject === "WEB") {
-        isWeb = true;
-      }
-      renderQuestions(questions, subject, isWeb);
+      allQuestions = await fetchJSONFile(filePath);
+      renderQuestions(allQuestions, subject, subject === "WEB");
       navLinks.classList.toggle("show");
-      // Remove active class from all buttons
       document
         .querySelectorAll(".nav-links li button")
         .forEach((btn) => btn.classList.remove("active"));
-      // Add active class to the clicked button
       document
         .querySelector(`.nav-links li button[data-subject="${subject}"]`)
         .classList.add("active");
     }
 
-    // Populate the navigation bar
+    function getSubjectTitle(subject) {
+      const subjectTitles = {
+        WEB: "WEB - Tehnologii Web",
+        ASD: "ASD - Algoritmi și Structuri de Date",
+        CC: "CC - Cloud Computing",
+        CE: "CE - Comerț Electronic",
+        "C++": "Programare C++",
+        CRIPTO: "CRIPTO - Criptografie",
+        DB: "DB - Baze de Date",
+        JAVA: "Programare Java",
+        PP: "PP - Programare Procedurală",
+        RC: "RC - Rețele de Calculatoare",
+        SGDB: "SGDB - Sisteme de Gestiune a Bazelor de Date",
+        SO: "SO - Sisteme de Operare",
+        TAP: "TAP - Tehnici Avansate de Programare",
+      };
+      return subjectTitles[subject] || subject;
+    }
+
+    function filterQuestions(query) {
+      const filteredQuestions = allQuestions.filter((question) => {
+        return question.question.toLowerCase().includes(query.toLowerCase());
+      });
+      const currentSubject = document
+        .querySelector(".nav-links li button.active")
+        .getAttribute("data-subject");
+      renderQuestions(
+        filteredQuestions,
+        currentSubject,
+        currentSubject === "WEB"
+      );
+    }
+
     const navLinks = document.querySelector(".nav-links");
-    Object.entries(subjects).forEach(([subject, filePath]) => {
+    Object.entries(subjects).forEach(([subject]) => {
       const li = document.createElement("li");
       const button = document.createElement("button");
       button.textContent = subject;
@@ -189,16 +167,16 @@
       navLinks.appendChild(li);
     });
 
-    // Add event listener for menu toggle (if clicked again, hide the navigation bar not delete it)
     const menuToggle = document.querySelector(".menu-toggle");
-    // set by default the navLinks to be hidden
     navLinks.classList.toggle("show");
     menuToggle.addEventListener("click", function () {
-      // Toggle the 'show' class to display or hide the navigation bar
       navLinks.classList.toggle("show");
     });
 
-    // Load the first subject by default
+    searchBox.addEventListener("input", (event) => {
+      filterQuestions(event.target.value);
+    });
+
     loadSubject(Object.keys(subjects)[0]);
   } catch (error) {
     console.error(error);
