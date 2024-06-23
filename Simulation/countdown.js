9ad8a26fc1ba6327b4398d7a466d4c59641af68e
iamxorum <this.isxorum@gmail.com>
2024-06-23 09:17:48 +0300

    23.06.2024 - Added Days

--- Simulation/countdown.js
+++ Simulation/countdown.js
@@ -1,4 +1,3 @@
-// Create Countdown
 var Countdown = {
   // Backbone-like structure
   $el: $(".countdown"),
@@ -11,6 +10,7 @@ var Countdown = {
   init: function () {
     // DOM
     this.$ = {
+      days: this.$el.find(".bloc-time.days .figure"),
       hours: this.$el.find(".bloc-time.hours .figure"),
       minutes: this.$el.find(".bloc-time.min .figure"),
       seconds: this.$el.find(".bloc-time.sec .figure"),
@@ -21,10 +21,11 @@ var Countdown = {
     var currentDate = new Date().getTime();
     var timeDifference = targetDate - currentDate;
 
-    // Calculate initial hours, minutes, and seconds
+    // Calculate initial days, hours, minutes, and seconds
     this.total_seconds = Math.floor(timeDifference / 1000);
     this.values = {
-      hours: Math.floor(this.total_seconds / 3600),
+      days: Math.floor(this.total_seconds / (3600 * 24)),
+      hours: Math.floor((this.total_seconds % (3600 * 24)) / 3600),
       minutes: Math.floor((this.total_seconds % 3600) / 60),
       seconds: this.total_seconds % 60,
     };
@@ -35,6 +36,8 @@ var Countdown = {
 
   count: function () {
     var that = this,
+      $day_1 = this.$.days.eq(0),
+      $day_2 = this.$.days.eq(1),
       $hour_1 = this.$.hours.eq(0),
       $hour_2 = this.$.hours.eq(1),
       $min_1 = this.$.minutes.eq(0),
@@ -44,19 +47,34 @@ var Countdown = {
 
     this.countdown_interval = setInterval(function () {
       if (that.total_seconds > 0) {
+        // Update seconds
         --that.values.seconds;
 
-        if (that.values.minutes >= 0 && that.values.seconds < 0) {
+        if (that.values.seconds < 0) {
           that.values.seconds = 59;
           --that.values.minutes;
-        }
 
-        if (that.values.hours >= 0 && that.values.minutes < 0) {
-          that.values.minutes = 59;
-          --that.values.hours;
+          if (that.values.minutes < 0) {
+            that.values.minutes = 59;
+            --that.values.hours;
+
+            if (that.values.hours < 0) {
+              that.values.hours = 23;
+              --that.values.days;
+
+              if (that.values.days < 0) {
+                // Countdown ended, do something here if needed
+                clearInterval(that.countdown_interval);
+                return;
+              }
+            }
+          }
         }
 
         // Update DOM values
+        // Days
+        that.checkHour(that.values.days, $day_1, $day_2);
+
         // Hours
         that.checkHour(that.values.hours, $hour_1, $hour_2);
 
@@ -93,9 +111,7 @@ var Countdown = {
       ease: Quart.easeOut,
       onComplete: function () {
         $top.html(value);
-
         $bottom.html(value);
-
         TweenMax.set($top, { rotationX: 0 });
       },
     });
@@ -109,21 +125,16 @@ var Countdown = {
   },
 
   checkHour: function (value, $el_1, $el_2) {
-    var val_1 = value.toString().charAt(0),
-      val_2 = value.toString().charAt(1),
+    var val_1 = Math.floor(value / 10),
+      val_2 = value % 10,
       fig_1_value = $el_1.find(".top").html(),
       fig_2_value = $el_2.find(".top").html();
 
-    if (value >= 10) {
-      // Animate only if the figure has changed
-      if (fig_1_value !== val_1) this.animateFigure($el_1, val_1);
-      if (fig_2_value !== val_2) this.animateFigure($el_2, val_2);
-    } else {
-      // If we are under 10, replace first figure with 0
-      if (fig_1_value !== "0") this.animateFigure($el_1, 0);
-      if (fig_2_value !== val_1) this.animateFigure($el_2, val_1);
-    }
+    // Animate only if the figure has changed
+    if (fig_1_value !== val_1.toString()) this.animateFigure($el_1, val_1);
+    if (fig_2_value !== val_2.toString()) this.animateFigure($el_2, val_2);
   },
 };
 
+// Initialize countdown
 Countdown.init();
